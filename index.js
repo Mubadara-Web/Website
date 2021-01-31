39d3e04f667bc3554e91608b0cfbccb1255d6e0e
oaltahan <omar.altahan96@gmail.com>
2021-01-31 21:06:22 +0300

    merge HomePage to backend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
-    res.render("landing");
+    res.render("home");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
