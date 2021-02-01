800222b87a1e2acfc434f5375f3aeb1b60875ba3
oaltahan <omar.altahan96@gmail.com>
2021-02-01 03:10:23 +0300

    add sign up logic

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser");
 
 mongoose.connect("mongodb+srv://admin:Mfz9iwmxBkCxRuD@cluster0.ohjzi.mongodb.net/Cluster0?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var User = require("./models/user")
 
 app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.static(__dirname + "/public"));
@@ -22,6 +24,24 @@ app.get("/login", function(req, res) {
 
 app.get("/register", function(req, res) {
     res.render("register");
+});
+// Test user
+// username: Test
+// email: test@test.test
+// password: pass
+
+// sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username, email: req.body.email });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            return res.redirect("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/");
+        });
+    });
+    console.log("new user registered");
 });
 
 app.get("/about", function(req, res) {
